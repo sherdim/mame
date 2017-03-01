af90a99fcb4cb8ebf5825367ddebd39f100d1af8
sherdim <sherdim@gmail.com>
2017-03-01 13:45:55 +0300

    test

--- custom.js
+++
@@ -1,6 +0,0 @@
-function coapp(){document.title+=" - PYLAB";};
-coapp();
-events.on("notebook_loaded.Notebook", function () {coapp()});
-events.on('notebook_saved.Notebook', function () {coapp()});
-events.on('notebook_renamed.Notebook', function () {coapp()});
-
