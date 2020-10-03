13ed812cab68faf293aa64fb6d97dd8b9f3623f3
Дим Щ <sherdim@gmail.com>
2020-10-03 07:58:46 +0300

    Create custom.js

---
+++ .jupyter/custom/custom.js
@@ -0,0 +1,5 @@
+function coapp(){document.title+=" - PYLAB";};
+coapp();
+events.on("notebook_loaded.Notebook", function () {coapp()});
+events.on('notebook_saved.Notebook', function () {coapp()});
+events.on('notebook_renamed.Notebook', function () {coapp()});
